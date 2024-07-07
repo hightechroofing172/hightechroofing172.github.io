85cc071daa09931fb88ddddcf2485da7c28424e8
hvacweb1234 <hvacwebcraft@gmail.com>
2024-07-07 15:17:07 +0100

    nwq

--- js/main.js
+++ js/main.js
@@ -11,34 +11,30 @@
     };
     spinner();
     
-    
     // Initiate the wowjs
     new WOW().init();
 
-
     // Sticky Navbar
-    window.addEventListener('scroll', function(e) {
-        if (window.scrollY > 300) {
-            $('.sticky-top').addClass('shadow-sm').css('top', '0px');
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 300) {
+            $('.sticky-top').addClass('shadow-sm').css('top', '-120px');
         } else {
             $('.sticky-top').removeClass('shadow-sm').css('top', '-100px');
         }
-    }, { passive: true });
-    
+    });
     
     // Back to top button
-    window.addEventListener('scroll', function() {
-        if (window.scrollY > 300) {
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 300) {
             $('.back-to-top').fadeIn('slow');
         } else {
             $('.back-to-top').fadeOut('slow');
         }
-    }, { passive: true });
+    });
     $('.back-to-top').click(function () {
         $('html, body').animate({scrollTop: 0}, 1500, 'easeInOutExpo');
         return false;
     });
-
 
     // Modal Video
     var $videoSrc;
@@ -53,90 +49,33 @@
         $("#video").attr('src', $videoSrc);
     })
 
-
     // Facts counter
     $('[data-toggle="counter-up"]').counterUp({
         delay: 10,
         time: 2000
     });
 
-
-    // Project carousel
-    $(".project-carousel").owlCarousel({
-        autoplay: true,
-        smartSpeed: 1000,
-        loop: true,
-        center: true,
-        dots: false,
-        nav: true,
-        navText : [
-            '<i class="bi bi-chevron-left"></i>',
-            '<i class="bi bi-chevron-right"></i>'
-        ],
-        responsive: {
-            0:{
-                items:2
-            },
-            576:{
-                items:2
-            },
-            768:{
-                items:3
-            },
-            992:{
-                items:4
-            },
-            1200:{
-                items:5
-            }
+ // Testimonials carousel
+ $(".testimonial-carousel").owlCarousel({
+    autoplay: true,
+    smartSpeed: 1000,
+    center: true,
+    dots: false,
+    loop: true,
+    nav : true,
+    navText : [
+        '<i class="bi bi-arrow-left"></i>',
+        '<i class="bi bi-arrow-right"></i>'
+    ],
+    responsive: {
+        0:{
+            items:1
+        },
+        768:{
+            items:2
         }
-    });
+    }
+});
 
 
-    // Testimonials carousel
-    $(".testimonial-carousel").owlCarousel({
-        autoplay: true,
-        smartSpeed: 1000,
-        center: true,
-        dots: false,
-        loop: true,
-        nav : true,
-        navText : [
-            '<i class="bi bi-arrow-left"></i>',
-            '<i class="bi bi-arrow-right"></i>'
-        ],
-        responsive: {
-            0:{
-                items:1
-            },
-            768:{
-                items:2
-            }
-        }
-    });
-
-    
 })(jQuery);
-
-// JavaScript to control the pop-up behavior
-
-// Show the pop-up after a delay (e.g., 10 seconds)
-setTimeout(function() {
-    showPopup();
-}, 10000);
-
-// Function to show the pop-up
-function showPopup() {
-    var popup = document.getElementById('popupContainer');
-    if (popup) {
-        popup.style.display = 'block';
-    }
-}
-
-// Function to close the pop-up
-function closePopup() {
-    var popup = document.getElementById('popupContainer');
-    if (popup) {
-        popup.style.display = 'none';
-    }
-}
